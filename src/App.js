c16576c46c3991a4e91ae2dff2827213e2ee1698
Nurbolat Sansyzbay <nurbolatsnk@gmail.com>
2025-02-25 12:07:13 +0500

    Added storing to localStorage logic for watched movies

--- src/App.js
+++ src/App.js
@@ -274,11 +274,14 @@ function ErrorMessage({ message }) {
 
 export default function App() {
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState();
+  const [watched, setWatched] = useState(function() {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => id === selectedId ? null : id );
@@ -295,6 +298,10 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched(watched => watched.filter(movie => movie.imdbID !== id));
   }
+
+  useEffect(function() {
+    localStorage.setItem('watched', JSON.stringify(watched));
+  }, [watched]); 
 
   useEffect(function() {
     const controller = new AbortController();
