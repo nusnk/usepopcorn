b7f7d249982fcc9ba68f46ced6e168c3d436b19d
Nurbolat Sansyzbay <nurbolatsnk@gmail.com>
2025-02-26 11:22:08 +0500

    Added feature to focus and clean the search on enter click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from './StarRating'
 const KEY = "526086fd";
 
@@ -13,12 +13,28 @@ function NavBar({ children }) {
 }
 
 function Search({ query, setQuery }) {
+  const inputRef = useRef(null);
+
+  useEffect(function() {
+    function callback(e) {
+      if(document.activeElement === inputRef.current) return;
+
+      if (e.code === "Enter") {
+        inputRef.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener("keydown", callback);
+    return () => document.removeEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
       type="text"
       placeholder="Search movies..."
       value={query}
+      ref={inputRef}
       onChange={(e) => setQuery(e.target.value)}
     />
   );
